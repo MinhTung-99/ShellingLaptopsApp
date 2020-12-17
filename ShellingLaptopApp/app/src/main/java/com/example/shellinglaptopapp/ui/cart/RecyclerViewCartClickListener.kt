63a0b2e3fc5857e9412c4b20e13c5081c98f855a
AndroidNative <tung07011999@gmail.com>
2020-12-17 09:21:25 +0700

    up

--- ShellingLaptopApp/app/src/main/java/com/example/shellinglaptopapp/ui/cart/RecyclerViewCartClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.shellinglaptopapp.ui.cart
-
-import com.example.shellinglaptopapp.data.model.Cart
-
-interface RecyclerViewCartClickListener {
-    fun orderOnClick(cart: Cart)
-    fun deleteOnClick(cart: Cart)
-}
--- ShellingLaptopApp/app/src/main/java/com/example/shellinglaptopapp/ui/laptops/RecyclerViewLaptopClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.shellinglaptopapp.ui.laptops
-
-import com.example.shellinglaptopapp.data.model.Laptop
-
-interface RecyclerViewLaptopClickListener {
-    fun onRecyclerViewItemClick(laptop: Laptop)
-}
