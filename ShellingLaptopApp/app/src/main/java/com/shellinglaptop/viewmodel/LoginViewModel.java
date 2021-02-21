94ca303103a98f4eaa83a4656bf4637a13b4bac8
MinhTung-99 <tung07011999@gmail.com>
2021-02-21 21:31:08 +0700

    update

--- ShellingLaptopApp/app/src/main/java/com/shellinglaptop/viewmodel/LoginViewModel.java
+++ ShellingLaptopApp/app/src/main/java/com/shellinglaptop/viewmodel/LoginViewModel.java
@@ -60,7 +60,7 @@ public class LoginViewModel extends ViewModel {
                     if(response.body() != null){
                         isProgressBar.postValue(false);
                         UserUtils.userName = userLogin.getUserName();
-                        UserUtils.password = userLogin.getPassword();
+                        UserUtils.password = userLogin.getPassword(); 
                         if(response.body().getTypeUser().equals(UserUtils.ADMIN)){
                             Navigation.findNavController(view).navigate(R.id.laptopAdminFragment);
                         }else if(response.body().getTypeUser().equals(UserUtils.USER)){
